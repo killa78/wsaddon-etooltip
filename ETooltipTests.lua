b92b3aa0ed851b42fdbe13134d3d717fa63944f4
Paul Prince <paul@littlebluetech.com>
2015-08-21 02:55:34 -0400

    Another attempt to show chat links.

--- ETooltipTests.lua
+++ ETooltipTests.lua
@@ -299,11 +299,13 @@ function TooltipTests:ItemToolTip (wndControl, item, bStuff, nCount)
         if this.tSettings["bShowItemID"] then
             if wndTooltip then
                 local ItemId = item:GetItemId()
-                this:AttachBelow(string.format("Item ID: %d %x", ItemId, ItemId), wndTooltip:FindChild("ItemTooltip_Header"))
+                this:AttachBelow(string.format("Item ID: %d <i%x>", ItemId, ItemId), wndTooltip:FindChild("ItemTooltip_Header"))
+                -- this:AttachBelow("Item ID: " .. item:GetItemId() , wndTooltip:("ItemTooltip_Header"))
             end
             if wndTooltipComp then
-                local ItemId = bStuff.itemCompare:GetItemId()
-                this:AttachBelow(string.format("Item ID: %d %x", ItemId, ItemId), wndTooltipComp:FindChild("ItemTooltip_Header"))
+                local ItemId = bStuff.itemCompare:GetItemIds
+                this:AttachBelow(string.format("Item ID: %d <i%x>", ItemId, ItemId), wndTooltipComp:FindChild("ItemTooltip_Header"))
+                -- this:AttachBelow("Item ID: " .. bStuff.itemCompare:GetItemId() , wndTooltipComp:FindChild("ItemTooltip_Header"))
             end
         end
         --Linked item check for mouseover - we put compare into the permanant tooltip, so we ignore the mousever one
